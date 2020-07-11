2c1935bbd8594fc70e2fbd2a10642e021ac6e50a
DİYAR <diyarbjk44@gmail.coo>
2020-07-11 16:03:18 +0300

    simple arry

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
-        int sayi1=220;
-        int sayi2=284;
-        int toplam1=0;
-        int toplam2=0;
-        for (int i=1; i<sayi1; i++){
-            if (sayi1 % i == 0)
-                toplam1=toplam1 + i;
-        }
-        for (int i=1; i<sayi2; i++) {
-            if (sayi2 % i == 0)
-                toplam2 = toplam2 + i;
-        }
-        if (sayi1==toplam2 && sayi2==toplam1){
-            System.out.println("Bu iki sayı arkadaştır");
-        }else {
-            System.out.println("Bu iki sayı arkadaş değildir");
-        }
+       int [] sayilar = new int[] { 1,2,5,6,7,3,8};
+       int aranacak =5;
+       boolean varmi = false;
+       for (int sayi:sayilar){
+            if (sayi== aranacak){
+                varmi=true;
+                break;
+            }
+       }
+       if (varmi){
+           System.out.println("Sayı vardır");
+       }else {
+           System.out.println("Sayı mevcut değildir");
+       }
+
     }
+
 }
